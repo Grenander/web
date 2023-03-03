a2a7c33426ae1a3c6981a5b6751bbfe333fbb354
Mia1251 <svanteomax@gmail.com>
2023-03-03 11:27:05 +0100

    Redone branch based on Main.

---
+++ .vscode/frontend/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
+import Admin from './Pages/Admin';
+import User from './Pages/User';
+import Home from './Pages/Home';
+
+function App() {
+  let component
+   switch (window.location.pathname) {
+    case "/":
+      component = <Home />
+      break;
+    case "/admin" :
+      component = <Admin />
+      break;
+    case "/user" :
+      component = <User />
+      break;    
+   
+    default:
+      break;
+   }
+  return (
+
+   <> <Navbar/>
+   <div className='container'>
+   {component}
+
+   </div>
+   </>
+  )
+}
+
+export default App;
---
+++ .vscode/frontend/src/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+    return <nav className='nav'>
+    <a href='/' className='site-title'>Brand Transparency</a>
+    <ul>
+        <li>
+            <a href='/admin'>Admin</a>
+        </li>
+        <li> 
+            <a href='/user'>User</a>
+        </li>
+    </ul>
+  </nav>
+}
+
+export default Navbar;
